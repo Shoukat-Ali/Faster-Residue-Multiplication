14fbb78392823f2a92cc961c879043b56273a6b8
Shoukat-Ali <shoukat.1983@gmail.com>
2017-08-09 18:25:50 +0300

    Create vb32_ws521.c

---
+++ 32-bit/P-521/vb32_ws521.c
@@ -0,0 +1,10 @@
+/* Test program for ws521 scalar point multiplication
+ This is NIST standard Weierstrass curve p-521
+ Fully Tested and debugged
+ Uses constant time method described by Bos et al. - http://eprint.iacr.org/2014/130
+ Cache safety thanks to ed25519
+ M.Scott 27/02/2015
+ We have replaced gmul() by TMV_product()
+ We have added rdtscp() to measure the clock cycles count as suggested by Paoloni; http://www.intel.com.tr/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
+ We have amended scr() and gsqr() according to modulus p
+ gcc -Wall -O3 vb32_ws521.c -o vb32_ws521.exe */
